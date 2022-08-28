cc9085d26581f350342916b6f68c435782409f60
mohammad alnshawaty <mnshawaty@gmail.com>
2022-08-28 20:52:33 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> BringAPK by M.nshawaty
       </footer>
     </>
   )
